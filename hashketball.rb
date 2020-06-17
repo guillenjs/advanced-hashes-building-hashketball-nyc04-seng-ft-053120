20d0e6d64bcf7afa2bca9d14580b664806922ad4
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-17 16:28:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,8 @@ def game_hash
                 assists: 12,
                 steals: 3,
                 blocks: 1,
-                slam_dunks: 1]
+                slam_dunks: 1],
+                [player_name: "Reggie Evans"]
   },
   away: {
     team_name: {},
