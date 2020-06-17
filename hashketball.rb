b93f98913f7fd030dc2058f2b4e3d4a49fc0c2df
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-17 15:56:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,9 @@ def game_hash
   {home: {
     team_name: {},
       colors: {},
-      players: [player_name: "Alan Anderson"]
+      players: [player_name: "Alan Anderson",
+                blocks: " "
+          ]
   },
   away: {
     team_name: {},
