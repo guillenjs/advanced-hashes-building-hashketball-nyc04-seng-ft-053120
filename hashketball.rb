f4d820b498b49fc854823e75f7616faddd5d9425
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-17 16:50:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,14 +30,14 @@ def game_hash
                               blocks: 1,
                               slam_dunks: 15},
                               {player_name: "Mason Plumlee",
-                                        number: " ",
-                                        shoe: " ",
-                                        points: " ",
-                                        rebounds: " ",
-                                        assists: " ",
-                                        steals: " ",
-                                        blocks: " ",
-                                        slam_dunks: " "}
+                                        number: 1,
+                                        shoe: 19,
+                                        points: 26,
+                                        rebounds: 11,
+                                        assists: 6,
+                                        steals: 3,
+                                        blocks: 8,
+                                        slam_dunks: 5}
                 ]
   },
   away: {
