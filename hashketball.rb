d2f146bf1586a6ec6599ff2609dd1e05c33f7796
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-17 16:24:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,7 @@ def game_hash
                 steals: 3,
                 blocks: 1,
                 slam_dunks: 1],
-                  [player_name: "Alan Anderson",
+                  [player_name: "Reggie Evans",
                           number: " ",
                           shoe: " ",
                           points: " ",
