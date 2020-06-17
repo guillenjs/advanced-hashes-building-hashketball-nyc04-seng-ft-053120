7da5bd92975d52fe1dc44ca4dc99755bcbadda52
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-17 16:58:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -70,7 +70,7 @@ def game_hash
                           steals: 22,
                           blocks: 15,
                           slam_dunks: 10},
-                          {player_name: "Desagna Diop",
+                          {player_name: "DeSagna Diop",
                                     number: " ",
                                     shoe: " ",
                                     points: " ",
